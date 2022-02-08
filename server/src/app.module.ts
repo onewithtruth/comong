26a94905ab78ffb90cd5168519bede4d4d66968b
dahyeon11 <dahyeon1712@dgu.ac.kr>
2022-02-08 17:42:14 +0900

    Signed-off-by: dahyeon11 <dahyeon1712@dgu.ac.kr>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { ItemsModule } from './modules/items/items.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ItemsModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
