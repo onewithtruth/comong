0067fc37cc387e37ae528a8f84a5b736b715aea6
Park Dahyeon <dahyeon1712@dgu.ac.kr>
2022-02-09 13:08:40 +0900

    API 문서 수정

--- server/src/modules/items/items.controller.ts
+++ server/src/modules/items/items.controller.ts
@@ -2,34 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('items')
-@ApiTags('items')
+@ApiTags('상품 정보 관련')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Post()
+  @ApiOperation({ summary: '새로운 상품 등록', description: '회원 가입 요청을 받습니다.' })
   create(@Body() createItemDto: CreateItemDto) {
     return this.itemsService.create(createItemDto);
   }
 
   @Get()
+  @ApiOperation({ summary: '상품 정보 목록', description: '회원 가입 요청을 받습니다.' })
+  @ApiCreatedResponse({ description: '유저를 생성한다.'})
   findAll() {
     return this.itemsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '상품 상세 정보', description: '회원 가입 요청을 받습니다.' })
   findOne(@Param('id') id: string) {
     return this.itemsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: '상품 정보 수정', description: '회원 가입 요청을 받습니다.' })
   update(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto) {
     return this.itemsService.update(+id, updateItemDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: '상품 삭제', description: '회원 가입 요청을 받습니다.' })
   remove(@Param('id') id: string) {
     return this.itemsService.remove(+id);
   }
