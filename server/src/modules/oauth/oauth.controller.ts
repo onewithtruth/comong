cf28ddfce1da431c5741d0aca8ace121070bde2d
Hojin Jang <onewithtruth@gmail.com>
2022-02-10 17:52:51 +0900

    update

--- server/src/modules/oauth/oauth.controller.ts
+++ server/src/modules/oauth/oauth.controller.ts
@@ -10,9 +10,23 @@ export class OauthController {
   constructor(private readonly oauthService: OauthService) {}
 
   @Post('oauthgoogle')
-  getRes(@Body() authorizationCode: OauthReqDto, @Response() res: any) {
+  getResfromGoogle(@Body() authorizationCode: OauthReqDto, @Response() res: any) {
     const code = authorizationCode.authorizationCode;
     const getTokenurl = `https://oauth2.googleapis.com/token?client_id=${process.env.GOOGLE_CLIENT_ID}&client_secret=${process.env.GOOGLE_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${process.env.GOOGLE_REDIRECT_URL}`;
     this.oauthService.googleOauthlogin(getTokenurl, res);
+  }
+
+  @Post('oauthkakao')
+  getResfromKakao(@Body() authorizationCode: OauthReqDto, @Response() res: any) {
+    const code = authorizationCode.authorizationCode;
+    const getTokenurl = `https://kauth.kakao.com/oauth/token?code=${code}&client_id=${process.env.KAKAO_CLIENT_ID}&client_secret=${process.env.KAKAO_CLIENT_SECRET}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}&grant_type=authorization_code`;
+    this.oauthService.kakaoOauthlogin(getTokenurl, res);
+  }
+
+  @Post('oauthnaver')
+  getResfromnaver(@Body() authorizationCode: OauthReqDto, @Response() res: any) {
+    const code = authorizationCode.authorizationCode;
+    const getTokenurl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.NAVER_CLIENT_ID}&client_secret=${process.env.NAVER_CLIENT_SECRET}&code=${code}`;
+    this.oauthService.naverOauthlogin(getTokenurl, res);
   }
 }
