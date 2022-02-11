02c2f772a973fad19b7f7d865011f909a44b6a23
Park Dahyeon <dahyeon1712@dgu.ac.kr>
2022-02-11 14:07:28 +0900

    Signed-off-by: Park Dahyeon <dahyeon1712@dgu.ac.kr>

--- server/src/util/swagger.ts
+++ server/src/util/swagger.ts
@@ -16,7 +16,30 @@ export function setupSwagger(app: INestApplication): void {
 
   const customOptions: SwaggerCustomOptions = {
     customCss: `
-    @import url('https://cdn.rawgit.com/innks/NanumSquareRound/master/nanumsquareround.min.css');
+    @font-face{
+      font-family:NanumSquareRound;
+      font-style:normal;
+      font-weight:300;
+      src: url(https://cdn.rawgit.com/innks/NanumSquareRound/master/NanumSquareRoundL.woff2)  format('woff2');
+    }
+    @font-face{
+      font-family:NanumSquareRound;
+      font-style:normal;
+      font-weight:500;
+      src: url(https://cdn.rawgit.com/innks/NanumSquareRound/master/NanumSquareRoundR.woff2)  format('woff2');
+    }
+    @font-face{
+      font-family:NanumSquareRound;
+      font-style:normal;
+      font-weight:700;
+      src: url(https://cdn.rawgit.com/innks/NanumSquareRound/master/NanumSquareRoundB.woff2)  format('woff2');
+    }
+    @font-face{
+      font-family:NanumSquareRound;
+      font-style:normal;
+      font-weight:800;
+      src: url(https://cdn.rawgit.com/innks/NanumSquareRound/master/NanumSquareRoundEB.woff2)  format('woff2');
+    }
     @font-face {
       font-family: 'Roboto Mono';
       font-style: normal;
@@ -31,31 +54,34 @@ export function setupSwagger(app: INestApplication): void {
       -moz-osx-font-smoothing: grayscale;
       -webkit-tap-highlight-color: transparent;
       line-height: calc(1ex / 0.32);
-      font-family: 'NanumSquareRound';
+      font-family: 'NanumSquareRound, 'sans-serif'';
     }
 
     .swagger-ui .topbar { display: none } 
     .swagger-ui .opblock .opblock-summary-operation-id, .swagger-ui .opblock .opblock-summary-path,.swagger-ui .opblock .opblock-summary-path__deprecated { font-family: 'NanumSquareRound', 'sans-serif' } 
-    .swagger-ui .opblock-tag { font-family: 'NanumSquareRound', 'sans-serif' }
+    .swagger-ui .opblock-tag { font-family: 'NanumSquareRound', 'sans-serif'; font-weight: 800 }
     .swagger-ui .opblock .opblock-summary-operation-id, .swagger-ui .opblock .opblock-summary-path, .swagger-ui .opblock .opblock-summary-path__deprecated { font-size: 16px; font-weight: 700 }
     .swagger-ui .opblock.opblock-post { border-color: #22cc90 }
     .swagger-ui svg:not(:root) { width: 15px; margin: 0 20px 0 0 }
-    .swagger-ui .opblock .opblock-summary-description { font-size: 13px; font-weight: 500; color: #444444 }
+    .swagger-ui .opblock .opblock-summary-description { font-size: 13.5px; font-weight: 500; font-family: 'NanumSquareRound', 'sans-serif'; color: #444444 }
     .swagger-ui .code, .swagger-ui code {  font-family: 'Roboto Mono', 'NanumSquareRound', 'sans-serif '; font-size: 14px; font-weight: 400}
-    .swagger-ui .response-col_status { font-family: 'NanumSquareRound', 'sans-serif'; font-size: 14px; font-weight: 700 }
+    .swagger-ui .response-col_status { font-family: 'NanumSquareRound', 'sans-serif'; font-size: 14px; font-weight: 800 }
     .swagger-ui .markdown p, .swagger-ui .markdown pre, .swagger-ui .renderedMarkdown p, .swagger-ui .renderedMarkdown pre { margin: 0; font-family: 'NanumSquareRound', 'sans-serif'; font-size: 14px; font-weight: 500 }
     .swagger-ui .response-col_description: { font-family: 'NanumSquareRound', 'sans-serif'; font-size: 16px; font-weight: 500 }
-    .swagger-ui .opblock .opblock-section-header h4 { font-family: inherit }
-    .swagger-ui .btn { font-family: inherit }
-    .swagger-ui select {  font-family: 'NanumSquareRound', 'sans-serif' }
-    .swagger-ui .parameter__type { font-family: inherit }
-    .swagger-ui .parameter__extension, .swagger-ui .parameter__in { font-family: inherit }
+    .swagger-ui .opblock .opblock-section-header h4 { font-family: 'NanumSquareRound', 'sans-serif'; font-size: 16px}
+    .swagger-ui .btn { font-family: 'NanumSquareRound', 'sans-serif' }
+    .swagger-ui select { font-family: 'NanumSquareRound', 'sans-serif' }
+    .swagger-ui .parameter__type { font-family: 'NanumSquareRound', 'sans-serif' }
+    .swagger-ui .parameter__extension, .swagger-ui .parameter__in { font-family: 'NanumSquareRound', 'sans-serif' }
     .swagger-ui .opblock.opblock-post .opblock-summary-method { background: #1cad6a }
     .swagger-ui .opblock.opblock-post .opblock-summary { border-color: #1cad6a }
     .swagger-ui .opblock.opblock-post { background: rgba(120,204,120,.1) }
     .swagger-ui .opblock.opblock-patch .opblock-summary-method { background: #00ad95 }
     .swagger-ui .opblock.opblock-patch .opblock-summary { border-color: #00ad95 }
     .swagger-ui .opblock.opblock-patch { background: rgba(20,227,194,.1) }
+    .swagger-ui .info .title {  font-family: 'NanumSquareRound', 'sans-serif'; font-weight: 800 }
+    .swagger-ui .opblock-description-wrapper p, .swagger-ui .opblock-external-docs-wrapper p, .swagger-ui .opblock-title_normal p {  font-family: 'NanumSquareRound', 'sans-serif'; font-weight: 500 }
+    .swagger-ui .response-col_links {  font-family: 'NanumSquareRound', 'sans-serif'; font-weight: 500 }
     `,
     customJs: `custom.js`,
     customSiteTitle: 'Comong Api Docs',
